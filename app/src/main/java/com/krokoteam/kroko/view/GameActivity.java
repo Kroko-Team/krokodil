5e8bbe9b9f4c8f67f4774a0db4fa01f24b418398
tetovske <tetovske@yandex.ru>
2020-04-26 02:24:54 +0300

    Pls rewrite logic from activity

--- app/src/main/java/com/krokoteam/kroko/view/GameActivity.java
+++ app/src/main/java/com/krokoteam/kroko/view/GameActivity.java
@@ -6,48 +6,103 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.util.Log;
+
 import io.agora.rtc.Constants;
 import io.agora.rtc.IRtcEngineEventHandler;
 import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
+import io.agora.rtc.video.VideoEncoderConfiguration;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 import com.krokoteam.kroko.R;
 
 public class GameActivity extends AppCompatActivity {
 
+    private RtcEngine mRtcEngine;
+
+    private FrameLayout mLocalContainer;
+    private SurfaceView mLocalView;
+
+    private RelativeLayout mRemoteContainer;
+    private SurfaceView mRemoteView;
+
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        // Listen for the onJoinChannelSuccess callback.
+        // This callback occurs when the local user successfully joins the channel.
+        public void onJoinChannelSuccess(String channel, final int uid, int elapsed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("agora","Join channel success, uid: " + (uid & 0xFFFFFFFFL));
+                }
+            });
+        }
+
+        @Override
+        // Listen for the onFirstRemoteVideoDecoded callback.
+        // This callback occurs when the first video frame of the broadcaster is received and decoded after the broadcaster successfully joins the channel.
+        // You can call the setupRemoteVideo method in this callback to set up the remote video view.
+        public void onFirstRemoteVideoDecoded(final int uid, int width, int height, int elapsed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("agora","First remote video decoded, uid: " + (uid & 0xFFFFFFFFL));
+                    setupRemoteVideo(uid);
+                }
+            });
+        }
+
+        @Override
+        // Listen for the onUserOffline callback.
+        // This callback occurs when the broadcaster leaves the channel or drops offline.
+        public void onUserOffline(final int uid, int reason) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("agora","User offline, uid: " + (uid & 0xFFFFFFFFL));
+                    // onRemoteUserLeft();
+                }
+            });
+        }
+    };
+
     private static final String LOG_TAG = GameActivity.class.getSimpleName();
     private static final int PERMISSION_REQ_ID = 1;
     private static final String[] REQUIRED_PERMISSIONS = {
             Manifest.permission.CAMERA,
-            Manifest.permission.RECORD_AUDIO
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
-        Button establishConnection = findViewById(R.id.connect_button);
-        establishConnection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkPermissions();
-            }
-        });
-
-        // mRtcEngine.enableVideo();
+        if (checkPermissions()) {
+            setupAgoraStreamingService();
+        } else {
+            Log.d(LOG_TAG, "YOU HAVE NO PERMISSIONS");
+        }
     }
 
-    private void checkPermissions() {
+    private boolean checkPermissions() {
         for (String permission : REQUIRED_PERMISSIONS) {
             if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSION_REQ_ID);
+                return false;
             }
         }
+        return true;
     }
 
     @Override
@@ -63,21 +118,63 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    //    private void initializeAgoraEngine() {
-//        try {
-//            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
-//        } catch (Exception e) {
-//            Log.e(LOG_TAG, Log.getStackTraceString(e));
-//            throw new RuntimeException("NEED TO check rtc sdk init fatal error" + Log.getStackTraceString(e));
-//        }
-//    }
+    private void setupAgoraStreamingService() {
+        try {
+            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, Log.getStackTraceString(e));
+            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
+        }
+    }
+
+    // TODO: убрать?
+    private void setChannelProfile() {
+        mRtcEngine.setChannelProfile(Constants.CHANNEL_PROFILE_LIVE_BROADCASTING);
+    }
+
+    // TODO: придумать как генерить код комнаты
+    public void forwardToLiveRoom(int cRole) {
+        // final EditText v_room = (EditText) findViewById(R.id.room_name);
+        // String room = v_room.getText().toString();
+        String roomID = "12345678";
+
+//        Intent i = new Intent(MainActivity.this, LiveRoomActivity.class);
+//        i.putExtra("CRole", cRole);
+//        i.putExtra("CName", room);
 //
-//    private void joinChannel() {
-//        // if you do not specify the uid, Agora will assign one.
-//        mRtcEngine.joinChannel(null, "demoChannel1", "Extra Optional Data", 0);
-//    }
-//
-//    private void leaveChannel() {
-//        mRtcEngine.leaveChannel();
-//    }
+//        startActivity(i);
+    }
+
+    // Изменить роль игрока (Загадывающий / угадывающий)
+    private void changeUserRole(int role) {
+        mRtcEngine.setClientRole(role);
+    }
+
+    private void setupLocalVideo() {
+        mRtcEngine.enableVideo();
+
+        mLocalView = RtcEngine.CreateRendererView(getBaseContext());
+        mLocalView.setZOrderMediaOverlay(true);
+        mLocalContainer.addView(mLocalView);
+        // Set the local video view.
+        VideoCanvas localVideoCanvas = new VideoCanvas(mLocalView, VideoCanvas.RENDER_MODE_HIDDEN, 0);
+        mRtcEngine.setupLocalVideo(localVideoCanvas);
+    }
+
+    private void setupRemoteVideo(int uid) {
+        mRemoteView = RtcEngine.CreateRendererView(getBaseContext());
+        mRemoteContainer.addView(mRemoteView);
+        // Set the remote video view.
+        mRtcEngine.setupRemoteVideo(new VideoCanvas(mRemoteView, VideoCanvas.RENDER_MODE_HIDDEN, uid));
+
+    }
+
+    private void joinChannel(String roomID, String token) {
+
+        // For SDKs earlier than v3.0.0, call this method to enable interoperability between the Native SDK and the Web SDK if the Web SDK is in the channel. As of v3.0.0, the Native SDK enables the interoperability with the Web SDK by default.
+        // mRtcEngine.enableWebSdkInteroperability(true);
+
+        // Join a channel with a token.
+        mRtcEngine.joinChannel(token, roomID, "Extra Optional Data", 0);
+    }
 }
