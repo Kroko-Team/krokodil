13cd41bc5ba255375d6f025db3717b24752e2c24
Igor <igor_barishpol@mail.ru>
2020-04-26 21:07:23 +0300

    Auth works. Some fixes need

--- app/src/main/java/com/krokoteam/kroko/view/MainActivity.java
+++ app/src/main/java/com/krokoteam/kroko/view/MainActivity.java
@@ -12,10 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.mainactivity_layout, AuthFragment.newInstance())
-                    .commit();
-        }
     }
 }
